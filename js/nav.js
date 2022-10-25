e287d3272e802cdee25975972f79bd0a8f135a7b
RominaGaite <gaiteromina@gmaul.com>
2022-10-24 23:28:55 -0300

    Se agrega el proeycto

---
+++ js/nav.js
@@ -0,0 +1,23 @@
+// Objeto Document
+// Métodos
+// Propiedad: innerHTML
+// Template String
+
+// esto no funcionó:
+document.getElementById("idheader").innerHTML= `
+<div><img src="img/logo1.jpeg" alt="miboda.com" width="70" height="70"></div>
+<div><h3>Miboda.com </h3></div>
+<div><a href="http://www.instagram.com" target="_blank"><img src="img/instagram.png" alt="instagram" width="40" height="40"></a>
+    <a href="http://www.twitter.com" target="_blank"><img src="img/twitter.png" alt="twitter" width="40" height="40"></a>
+    <a href="http://www.facebook.com" target="_blank"><img src="img/facebook.png" alt="facebook" width="40" height="40"></a>
+    <a href="http://www.whatsapp.com" target="_blank"><img src="img/whatsapp.png" alt="whatsapp" width="40" height="40"></div></a>
+  
+</div>
+<!-- Botones -->
+<div class="navbar">
+<a href="index.html">Página principal</a>
+<a href="galería.html">Trabajos realizados</a>
+<a href="preguntas.html">Preguntas frecuentes</a>
+<a href="contacto.html">Contáctenos</a>
+`; 
+
---
+++ js/valida.js
@@ -0,0 +1,20 @@
+
+//No hubo forma de que funcione.
+    
+      // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+      var forms = document.querySelectorAll('.needs-validation')
+    
+      //Recorremos los forms y evitamos en envío sin validacion
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+          }, false)
+        })
+
+
+ 
