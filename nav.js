0b5822cffbe8c94fc91a07f2b97a9145a25dc441
RominaGaite <gaiteromina@gmaul.com>
2022-10-30 21:42:25 -0300

    cambios 4

--- nav.js
+++ nav.js
@@ -7,10 +7,10 @@
 document.getElementById("idheader").innerHTML= `
 <div><img src="img/logo1.jpeg" alt="miboda.com" width="70" height="70"></div>
 <div><h3>Miboda.com </h3></div>
-<div><a href="http://www.instagram.com" target="_blank"><img src="img/instagram.png" alt="instagram" width="40" height="40"></a>
-    <a href="http://www.twitter.com" target="_blank"><img src="img/twitter.png" alt="twitter" width="40" height="40"></a>
-    <a href="http://www.facebook.com" target="_blank"><img src="img/facebook.png" alt="facebook" width="40" height="40"></a>
-    <a href="http://www.whatsapp.com" target="_blank"><img src="img/whatsapp.png" alt="whatsapp" width="40" height="40"></div></a>
+<div><a href="http://www.instagram.com" target="_blank"><img src="imagenes/ig.jpg" alt="instagram" width="40" height="40"></a>
+    <a href="http://www.twitter.com" target="_blank"><img src="imagenes/twtr.jpg" alt="twitter" width="40" height="40"></a>
+    <a href="http://www.facebook.com" target="_blank"><img src="imagenes/fb.jpg" alt="facebook" width="40" height="40"></a>
+    <a href="http://www.whatsapp.com" target="_blank"><img src="img/wsp.jpg" alt="whatsapp" width="40" height="40"></div></a>
   
 </div>
 <!-- Botones -->
